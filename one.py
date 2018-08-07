4f0648fc951df8a6ffff84a8f478173a5f1a5502
Shreyas Somashekar <shreyas0906@gmail.com>
2018-08-07 07:21:28 -0700

    first commit

---
+++ one.py
@@ -0,0 +1,24 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from PIL import Image
+import caffe
+import cv2
+
+
+# caffe.set_mode_cpu()
+caffe.set_device(0)
+caffe.set_mode_gpu()
+
+net = caffe.Net('conv.prototxt', caffe.TEST)
+
+
+im = cv2.imread('test-image.jpeg',0)
+im_input = im[np.newaxis, np.newaxis, : ,: ]
+
+net.blobs['data'].reshape(*im_input.shape)
+net.blobs['data'].data[...] = im_input
+
+print [(k, v.data.shape) for k, v in net.blobs.items()]
+
+net.forward()
+
